45c22d1c30068c47c3c393c0e6e7bed32d5300b9
washynator <washynator@gmail.com>
2019-02-15 23:56:00 +0200

    Little refactoring, added rooms

---
+++ TheHorror/KeyWords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHorror
+{
+    public static class KeyWords
+    {
+        public static string[] keyWords = {
+            "quit",
+            "north",
+            "east",
+            "south",
+            "west",
+            "prompt"
+        };
+    }
+}
--- TheHorror/Program.cs
+++ TheHorror/Program.cs
@@ -3,33 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace TheHorror
 {
-
-    public static class KeyWords
+    public class Room
     {
-        public static string[] keyWords = { "quit", "north", "east", "south", "west" };
+        public List<string> Exits = new List<string>();
     }
 
     class Program
     {
-        static string Prompt = ">> ";
+        static string PlayerPrompt = ">> ";
+        static string PlayerName = "";
+        static Room TestRoom = new Room();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to THE HORROR!");
-            Console.Write("Please enter your name, before entering the house of horrors: ");
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Okay, " + name + " we are happy to have you here. I'm sure you won't be so happy. BWAHAHAHAHA!");
+            StartGame();
 
             do
             {
-                Console.WriteLine(Prompt + GetInput(Console.ReadLine()));
+                PrintExits();
+                PrintPrompt();
             } while (GetInput(Console.ReadLine()) != KeyWords.keyWords[0]);
+        }
 
+        static void StartGame()
+        {
+            TestRoom.Exits.Add("north");
+            TestRoom.Exits.Add("west");
+
+            Console.WriteLine("Welcome to THE HORROR!");
+            Console.Write("Please enter your name, before entering the house of horrors: ");
+            PlayerName = Console.ReadLine();
+            Console.WriteLine("Okay, " + PlayerName + " we are happy to have you here. I'm sure you won't be so happy. BWAHAHAHAHA!");
+        }
+
+        static void PrintPrompt()
+        {
+            Console.Write(PlayerPrompt);
+        }
+
+        static void PrintExits()
+        {
+            foreach(string exit in TestRoom.Exits)
+            {
+                Console.Write("(" + exit + ") ");
+            }
+        }
+
+        static void EditPrompt()
+        {
+            Console.Write("Now you can input any kind of prompt you would like (space will not be added): ");
+            PlayerPrompt = Console.ReadLine();
+        }
+
+        static bool CheckExits(string direction)
+        {
+            return TestRoom.Exits.Contains(direction);
         }
 
         public static string GetInput(string playerInput)
@@ -45,20 +76,53 @@ namespace TheHorror
                 {
                     switch (command[i])
                     {
+                        case "quit":
+                            return "quit";
+                        case "exit":
+                            return "quit";
                         case "north":
-                            Console.WriteLine("Moving north");
+                            if (CheckExits("north"))
+                            {
+                                Console.WriteLine("Moving north");
+                            }
+                            else
+                            {
+                                Console.WriteLine("That way is blocked");
+                            }
                             break;
                         case "east":
-                            Console.WriteLine("Moving east");
+                            if (CheckExits("east"))
+                            {
+                                Console.WriteLine("Moving east");
+                            }
+                            else
+                            {
+                                Console.WriteLine("That way is blocked");
+                            }
                             break;
                         case "south":
-                            Console.WriteLine("Moving south");
+                            if (CheckExits("south"))
+                            {
+                                Console.WriteLine("Moving south");
+                            }
+                            else
+                            {
+                                Console.WriteLine("That way is blocked");
+                            }
                             break;
                         case "west":
-                            Console.WriteLine("Moving west");
+                            if (CheckExits("west"))
+                            {
+                                Console.WriteLine("Moving west");
+                            }
+                            else
+                            {
+                                Console.WriteLine("That way is blocked");
+                            }
                             break;
-                        case "quit":
-                            Console.WriteLine("Quitting.....");
+                        case "prompt":
+                            Console.WriteLine("Your current prompt is: " + PlayerPrompt);
+                            EditPrompt();
                             break;
                         default:
                             break;
@@ -66,7 +130,7 @@ namespace TheHorror
                     return command[i];
                 }
             }
-            return Prompt;
+            return PlayerPrompt;
         }
     }
 }
